43ae4f4881cb3d77a48b848d052114a2681e9268
olegmyltsev <111868242+olegmyltsev@users.noreply.github.com>
2023-04-19 22:09:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// const form = document.getElementsByClassName("form")
+// const overlay = document.getElementById("overlay")
+// const modal = document.getElementsByClassName("modal")
+
+function massage(){fetch('https://api.telegram.org/bot6130561127:AAGc09-QEuhaIX-LFecXFsgLBWsKGf4ih08/sendMessage?chat_id=895468913&parse_mode=html&text=Продай колеса')}
