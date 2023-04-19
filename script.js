bf20003881776015c4758b5a47fc0695ac48fbc7
olegmyltsev <111868242+olegmyltsev@users.noreply.github.com>
2023-04-19 22:18:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@
 // const overlay = document.getElementById("overlay")
 // const modal = document.getElementsByClassName("modal")
 
-function massage(){fetch('https://api.telegram.org/bot6130561127:AAGc09-QEuhaIX-LFecXFsgLBWsKGf4ih08/sendMessage?chat_id=-1001766510135&parse_mode=html&text=Продай колеса')}
+function massage(){fetch('https://api.telegram.org/bot6130561127:AAGc09-QEuhaIX-LFecXFsgLBWsKGf4ih08/sendMessage?chat_id=-1001766510135&parse_mode=html&text=Привет, я робот')}
